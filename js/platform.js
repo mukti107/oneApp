1cb363f287850f613863fa73a8f677063c410a04
Rachindra Poudel <Rachindra Poudel>
2017-08-07 21:27:37 +0545

    file

--- js/platform.js
+++ js/platform.js
@@ -17,7 +17,7 @@ var styles= [
 				"vendor/framework7/css/"+platform+"/colors.min.css",
 				"vendor/framework7/css/"+platform+"/min.css",
 				"vendor/framework7/css/icons.css",
-				]
+				];
 
 
 
