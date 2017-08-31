f94bda980c51ca62adea40b2e7b2ee7ca3a15e8c
Rachindra Poudel <Rachindra Poudel>
2017-08-31 13:03:42 +0545

    news

--- templates/listview/default.js
+++ templates/listview/default.js
@@ -1,7 +1,10 @@
 define({
 	template:` <ul>
 					{{#items}}
-						<li><a href="{{link}}">{{title}}</a></li>
+						<li><a href="{{link}}" traget="_blank"><h2>{{title}}</h2></a></li>
+						<hr>
+						<li><a href="{{link}}"><h2>{{title}}</h2></a></li>
+						<hr><hr>
 					{{/items}}
 				</ul>
 			`
