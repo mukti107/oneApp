4785c7e14644676830bd67b523f2277d48c5b135
Rachindra Poudel <Rachindra Poudel>
2017-09-01 08:53:05 +0545

    scroll

--- templates/listview/default.js
+++ templates/listview/default.js
@@ -1,17 +1,14 @@
 define({
-	template:` <div class="list-block ">
-					<ul>
-						{{#items}}
-						<li class="item-content">
-							<a href="{{link}}_newPage" >
-					          <div class="item-inner">
-					            <div class="item-title">{{title}}</div>
-					          </div>
-				         	</a>
-				        </li>
-							
-						{{/items}}
-					</ul>
-				</div>
+	template:`<ul>
+				{{#items}}
+				<li >
+					<a class="item-link item-content" href="{{link}}_newPage" >
+			          <div class="item-inner">
+			            <div class="item-title">{{title}}</div>
+			          </div>
+		         	</a>
+		        </li>
+				{{/items}}
+			</ul>
 			`
 })
