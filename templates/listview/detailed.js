375163ce0637cfad601e0239b3bc7288760c14fc
Rachindra Poudel <Rachindra Poudel>
2017-09-01 09:15:17 +0545

    scroll

---
+++ templates/listview/detailed.js
@@ -0,0 +1,19 @@
+define({
+	template:`<ul>
+				{{#items}}
+				<li >
+					<a href="#" class="item-link item-content">
+				        <div class="item-inner">
+				          <div class="item-title-row">
+				            <div class="item-title">{{title}}</div>
+				            <div class="item-after">{{pubDate}}</div>
+				          </div>
+				          <div class="item-subtitle">{{categories}} | {{author}}</div>
+				          <div class="item-text">{{description}}</div>
+				        </div>
+			      	</a>
+		        </li>
+				{{/items}}
+			</ul>
+			`
+})
