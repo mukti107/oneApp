73c9bc3d779016414369075a6352a964a89b4981
Rachindra Poudel <Rachindra Poudel>
2017-09-07 15:51:13 +0545

    forex

--- templates/forex.js
+++ templates/forex.js
@@ -28,28 +28,42 @@ define({
 				</div>
 				<div class="data-table card">
 				<div class="card-header">Open Market Exchange Rates</div>
-					<table>
-						<thead>
-							<tr>
-								<th class="label-cell">Currency</th>
-								<th class="numeric-cell">Unit</th>
-								<th class="numeric-cell">B.</th>
-								<th class="numeric-cell">S.</th>
-							</tr>
-						</thead>
-						{{#openMarketRate}}
-							<tr>
-								<td class="label-cell">{{currency}}</td>
-								<td class="numeric-cell">{{unit}}</td>
-								<td class="numeric-cell">{{buyingRate}}</td>
-								<td class="numeric-cell">{{sellingRate}}</td>
-							
-							</tr>
-						{{/openMarketRate}}
-					</table>
+					<div class="content-block">
+					{{#openMarketRate}}
+						<div class="row no-gutter">
+					        <div class="col-25">25%</div>
+					        <div class="col-25">25%</div>
+					        <div class="col-25">25%</div>
+					        <div class="col-25">25%</div>
+				      	</div>
+			      	{{/openMarketRate}}
+			      	</div>
+					
 				</div>
 			`
 })
+
+
+
+// <table>
+// 						<thead>
+// 							<tr>
+// 								<th class="label-cell">Currency</th>
+// 								<th class="numeric-cell">Unit</th>
+// 								<th class="numeric-cell">Buy</th>
+// 								<th class="numeric-cell">Sell</th>
+// 							</tr>
+// 						</thead>
+// 						{{#openMarketRate}}
+// 							<tr>
+// 								<td class="label-cell">{{currency}}</td>
+// 								<td class="numeric-cell">{{unit}}</td>
+// 								<td class="numeric-cell">{{buyingRate}}</td>
+// 								<td class="numeric-cell">{{sellingRate}}</td>
+							
+// 							</tr>
+// 						{{/openMarketRate}}
+// 					</table>
 
 
 
