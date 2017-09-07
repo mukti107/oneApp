e5399647458f2442d12b7d7e95aa7e7485a2ddf7
Rachindra Poudel <Rachindra Poudel>
2017-09-07 13:55:57 +0545

    forex

--- templates/forex.js
+++ templates/forex.js
@@ -1,13 +1,33 @@
 define({
 	template:`
 				<div class="data-table card">
+					<div class="card-header">Exchange Rates Fixed by Nepal Rastra Bank</div>
+					
+							
 					<table>
-						<tr>
-							{{#fixedRate}}
-
-							{{/fixedRate}}
-						</tr>
+						<thead>
+							<tr>
+								<th class="label-cell">Currency</th>
+								<th class="numeric-cell">Unit</th>
+								<th class="numeric-cell">Buying rate</th>
+								<th class="numeric-cell">Selling Rate</th>
+							</tr>
+						</thead>
+						{{#fixedRate}}
+							<tr>
+								<td class="label-cell">{{currency}}</td>
+								<td class="numeric-cell">{{unit}}</td>
+								<td class="numeric-cell">{{buyingRate}}</td>
+								<td class="numeric-cell">{{sellingRate}}</td>
+							
+							</tr>
+						{{/fixedRate}}
 					</table>
+							
+						
+				</div>
+				<div class="data-table card">
+				<div class="card-header">Open Market Exchange Rates</div>
 					<table>
 						<thead>
 							<tr>
