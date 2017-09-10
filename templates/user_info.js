8aad34d6570d9c786bb8afd04c567b1994e9c677
Rachindra Poudel <Rachindra Poudel>
2017-09-10 19:49:56 +0545

    styles

--- templates/user_info.js
+++ templates/user_info.js
@@ -2,9 +2,9 @@ define({
 	template:`
 				{{#logged}}
 				<a href="profile.html_newPage">
-					<div  style="line-height:40px;vertical-align: middle;float:left; margin-right:5px;">{{name}}
+					<div  style="line-height:32px;vertical-align: middle;float:left; margin-right:5px;">{{name}}
 					</div>
-					<img style="border-radius:1000px;" src="{{picture}}" width="40" />
+					<img style="border-radius:16px;" src="{{picture}}" width="32" />
 				{{/logged}}
 				{{^logged}}	
            	   		<a href="oneapp://facebookLogin"><i class="icon f7-icons">social_facebook_fill</i></a>
