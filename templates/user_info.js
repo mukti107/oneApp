af49003cdfa27bce4aa172dda65b0f0cad1acbc2
Rachindra Poudel <Rachindra Poudel>
2017-09-10 22:33:59 +0545

    styles

--- templates/user_info.js
+++ templates/user_info.js
@@ -2,7 +2,7 @@ define({
 	template:`
 				{{#logged}}
 				<a href="profile.html_newPage" >
-					<div  style="line-height:32px;vertical-align: middle;float:left; margin-right:5px;color:#fff;">{{name}}
+					<div  style="line-height:32px;vertical-align: middle;float:left; margin-right:5px;color:#666;">{{name}}
 					</div>
 					<img style="border-radius:16px;" src="{{picture}}" width="32" />
 				{{/logged}}
