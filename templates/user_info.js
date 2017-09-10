66f6e267ccb9fa3fddfd4815f4002ae770fdfa5a
Rachindra Poudel <Rachindra Poudel>
2017-09-10 19:29:43 +0545

    top_bar

--- templates/user_info.js
+++ templates/user_info.js
@@ -1,8 +1,14 @@
 define({
 	template:`
-				<span style="line-height:40px;vertical-align: middle;">{{name}}
-				</span>
-				<img style="border-radius:1000px;" src="{{picture}}" width="20" />
+				{{#logged}}
+				<a href="profile.html_newPage">
+					<div  style="line-height:40px;vertical-align: middle;float:left; margin-right:5px;">{{name}}
+					</div>
+					<img style="border-radius:1000px;" src="{{picture}}" width="40" />
+				{{/logged}}
+				{{^logged}}	
+           	   		<a href="oneapp://facebookLogin"><i class="icon f7-icons">social_facebook_fill</i></a>
+				{{/logged}}
 				
 			`
 })
