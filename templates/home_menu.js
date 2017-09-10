34de353da9569347b0faf7ed5782c9191c0c9a81
Rachindra Poudel <Rachindra Poudel>
2017-09-10 22:19:58 +0545

    styles

--- templates/home_menu.js
+++ templates/home_menu.js
@@ -1,8 +1,8 @@
 define({
 	template:`
-				<div class="row">
+				<div class="row no-gutter">
 				    {{#items}}
-						<div class=" col-33 home_menu_item text-center" style="position:relative;">
+						<div class=" col-33 home_menu_item text-center" style="position:relative;background-color:{{bgColor}}">
 							<div class="absolute-center">
 							<a href="{{link}}" style="padding:15px 0 15px 0;color:#fff;">
 								<i class="icon f7-icons">{{icon}}</i><br/>
