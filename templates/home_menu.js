c30c8c3fd0beb24aa39a4f29d8528813185c2f60
Rachindra Poudel <Rachindra Poudel>
2017-09-11 11:55:03 +0545

    unicode

--- templates/home_menu.js
+++ templates/home_menu.js
@@ -2,14 +2,16 @@ define({
 	template:`
 				<div class="row no-gutter">
 				    {{#items}}
-						<div class=" col-33 home_menu_item text-center" style="position:relative;background-color:{{bgColor}}">
-							<div class="absolute-center">
-							<a href="{{link}}" style="padding:15px 0 15px 0;color:#fff;">
-								<i class="icon f7-icons">{{icon}}</i><br/>
-								{{name}}
-							</a>
+				    	{{#showInHome}}
+							<div class=" col-33 home_menu_item text-center" style="position:relative;background-color:{{bgColor}}">
+								<div class="absolute-center">
+								<a href="{{link}}" style="padding:15px 0 15px 0;color:#fff;">
+									<i class="icon f7-icons">{{icon}}</i><br/>
+									{{name}}
+								</a>
+								</div>
 							</div>
-						</div>
+						{{/showInHome}}
 					{{/items}}
 				</div> 
 				
