4476f77091ceee0bc91e3efc22c00f41b7467e0e
Rachindra Poudel <Rachindra Poudel>
2017-09-15 12:35:33 +0545

    dashboard

--- js/platform.js
+++ js/platform.js
@@ -29,7 +29,6 @@ var device = window.navigator.platform;
 var iosPlatforms = ['iPhone', 'iPad', 'iPod'];
 if(iosPlatforms.indexOf(device)!=-1)
 	platform="ios";
-platform="ios";
 
 ROOT_DIR=document.currentScript.src.split("js")[0];
 
